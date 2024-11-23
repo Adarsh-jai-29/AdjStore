e2f862d43c4b44faaee2b2197a350a7963689d32
adarsh <adarshjai29@gmail.com>
2024-11-23 19:56:05 +0530

    create localStorage for cart Products

--- src/components/context/CartContext.js
+++ src/components/context/CartContext.js
@@ -1,30 +1,50 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import reducer from "../reducer/cartReducer";
-export const CartContext = createContext()
+export const CartContext = createContext();
+
+const getCartData = function() {
+ const myStoredCartData = localStorage.getItem("myCartData")
+  if (myStoredCartData){
+    console.log(myStoredCartData)
+   return JSON.parse(myStoredCartData)
+  }
+  else{
+ return []
+  }
+
+}
 
 const InitialState = {
-  cartData :[],
-  totalItems : '',
-  totalAmount : '',
-  shippingFee : 5000,
-}
+  cartData: getCartData(),
+  totalItems: "",
+  totalAmount: "",
+  shippingFee: 5000,
+};
 
-export const CartProvider = ({children})=>{
-  
- const [state,dispatch] = useReducer(reducer,InitialState)
+export const CartProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, InitialState);
 
- const addToCart = (amount,product)=>{
-    console.log(amount)
-    console.log(product)
- 
-return dispatch({type:'add to cart',payload:{product,amount}})
- }
- const removeItem = (id)=>{
-  console.log(id)
-  return  dispatch({type:'remove cart item', payload : id})}
+  const addToCart = (amount, product) => {
+    console.log(amount);
+    console.log(product);
+    dispatch({ type: "add to cart", payload: { product, amount } });
+  };
+  const removeItem = (id) => {
+    console.log(id);
+    return dispatch({ type: "remove cart item", payload: id });
+  };
 
-  const clearCart = ()=>dispatch({type:'clear cart all items'})
+  const clearCart = () => dispatch({ type: "clear cart all items" });
+  useEffect(
+    () => localStorage.setItem("myCartData", JSON.stringify(state.cartData)),
+    [state.cartData]
+  );
 
-
- return <CartContext.Provider value={{...state,addToCart,removeItem,clearCart}} >{children}</CartContext.Provider>
-}
+  return (
+    <CartContext.Provider
+      value={{ ...state, addToCart, removeItem, clearCart }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
